ab2f79f039dfc54ab03051b705106c1d0c19eba0
schatten <schatten@mail.fms.nrw>
2026-08-01 18:11:31 +0200

    added command line arguments & parsing;

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use axum::routing::any;
+use clap::Parser;
+use std::path::PathBuf;
 use tracing::level_filters::LevelFilter;
 
 #[tokio::main]
@@ -7,7 +9,9 @@ async fn main() {
         .with_max_level(LevelFilter::TRACE)
         .init();
 
-    let server = server::Server::new("config.toml".into());
+    let args = Args::parse();
+
+    let server = server::Server::new(args.config_file);
     server.add_component::<default_components::WebsiteStatuse>()
         .add_component::<default_components::Frontend>()
         .add_component::<default_components::DataminerStatus>()
@@ -17,7 +21,22 @@ async fn main() {
     let router = axum::Router::new()
         .route("/", any(server.clone()))
         .route("/{*any}", any(server.clone()));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind((args.host, args.port)).await.unwrap();
     axum::serve(listener, router).await.unwrap();
+}
 
+#[derive(clap::Parser, Debug)]
+#[command(version, about="a custom status server")]
+struct Args {
+    /// The path of the config file
+    #[arg(short, long, alias="config", default_value="config.toml")]
+    config_file: PathBuf,
+
+    /// The host (*excluding port*) to bind to.
+    #[arg(short='b', long="bind", alias="host", default_value="0.0.0.0")]
+    host: String,
+
+    /// The port to bind to.
+    #[arg(short, long, default_value_t=5000)]
+    port: u16,
 }
